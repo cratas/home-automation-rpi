35a57fd80837ed0a595837792c9fa775112d294d
Petr Kratochvíl <56606404+cratas@users.noreply.github.com>
2023-03-18 17:20:52 +0100

    Update models.py

--- home_automation/backend/models.py
+++ home_automation/backend/models.py
@@ -171,10 +171,6 @@ class PullDevice(Device):
         PARAMETRES = 'parametres'
     format = models.CharField(max_length=20, choices=FORMATS.choices, default=FORMATS.choices[0])
     delimiter = models.CharField(max_length=1, default=',')
-    
-    
-    def save_data(self, data):
-        print("random output")
 
 # ----------    
 # DEVICE VALUES LIST MODEL
